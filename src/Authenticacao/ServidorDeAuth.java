000f2506defbe3b099ed18525fb2ba6f21f05033
oooyeee <iam.yaroslav@gmail.com>
2025-12-05 13:59:43 +0000

    pasta testes

---
+++ src/Authenticacao/ServidorDeAuth.java
@@ -0,0 +1,12 @@
+package Authenticacao;
+
+import servidor.SecureServer;
+
+public class ServidorDeAuth extends SecureServer {
+
+    public ServidorDeAuth(int port) {
+        super(port);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
--- test.ps1
+++ test.ps1
@@ -3,4 +3,4 @@ $java_files = (gci -Recurse -Include *.java src).FullName
 
 javac -Xdiags:verbose -Xlint:unchecked -d out $java_files
 #java -cp out Server
-java -cp out Test3
+java -cp out testes.TesteA
