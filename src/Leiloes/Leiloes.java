7db765c26469ecdcf2b9657e7abc259584abffbc
oooyeee <iam.yaroslav@gmail.com>
2025-12-05 13:52:57 +0000

    servidor de leiloes aqui

--- src/Leiloes/Leiloes.java
+++ src/Leiloes/Leiloes.java
@@ -1,5 +1,12 @@
 package Leiloes;
 
-public class Leiloes {
+import servidor.SecureServer;
+
+public class Leiloes extends SecureServer {
+
+    public Leiloes(int port) {
+        super(port);
+        //TODO Auto-generated constructor stub
+    }
     
 }
